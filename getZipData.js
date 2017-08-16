1e849c14ba830cf1dcb269fc726538a2e16ed71e
Purag Moumdjian <purmou@gmail.com>
2017-08-16 03:53:25 -0700

    add support for command line arguments and error checking for zip codes

--- getZipData.js
+++ getZipData.js
@@ -1,24 +1,34 @@
 const fs = require('fs');
 const { get } = require('request');
 
-fs.readFile('zips', 'utf-8', (err, data) => {
-  const zips = data.trim().split('\n');
-  const total = zips.length;
-  let done = 0;
+let total;
+let done = 0;
 
-  for (let zip of zips) {
-    (function (zip) {
-      get({
-        url: `https://www.moviepass.com/theaters/zip/${zip}`,
-        json: true,
-      }, (err, res, data) => {
-        const file = `data/${zip}.theaters.json`;
-        fs.writeFile(file, JSON.stringify(data, null, 2), (err) => {
-          if (err) throw err;
-          done++;
-          console.log(`${done}/${total}\tfetched theaters for ${zip} => ${file}`);
-        });
-      });
-    })(zip);
-  }
-});
+function fetchZipData (zip) {
+  if (!zip.match(/^[0-9]{5}$/)) throw new Error(`Invalid Zip Code: ${zip}`)
+
+  get({
+    url: `https://www.moviepass.com/theaters/zip/${zip}`,
+    json: true,
+  }, (err, res, data) => {
+    const file = `data/${zip}.theaters.json`;
+    fs.writeFile(file, JSON.stringify(data, null, 2), (err) => {
+      if (err) throw err;
+      console.log(`${++done}/${total}\tfetched theaters for ${zip} => ${file}`);
+    });
+  });
+}
+
+let zips;
+
+if (process.argv.length > 2) {
+  zips = process.argv.slice(2);
+} else {
+  const data = fs.readFileSync('zips', 'utf-8');
+  zips = data.trim().split('\n');
+}
+
+total = zips.length;
+for (let zip of zips) {
+  fetchZipData(zip);
+}
