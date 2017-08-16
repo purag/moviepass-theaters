b74d84bb599a6b5526f0b35b143d1b10788b0f70
Purag Moumdjian <purmou@gmail.com>
2017-08-16 03:29:05 -0700

    switch to request package for ease of handling json

--- getZipData.js
+++ getZipData.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { get } = require('https');
+const { get } = require('request');
 
 fs.readFile('zips', 'utf-8', (err, data) => {
   const zips = data.trim().split('\n');
@@ -8,14 +8,15 @@ fs.readFile('zips', 'utf-8', (err, data) => {
 
   for (let zip of zips) {
     (function (zip) {
-      get(`https://www.moviepass.com/theaters/zip/${zip}`, (res) => {
-        res.on('data', (data) => {
-          data = JSON.stringify(JSON.parse(data), null, 2);
-          fs.writeFile(`data/${zip}.theaters.json`, data, (err) => {
-            if (err) throw err;
-            done++;
-            console.log(`${done}/${total}\tfetched theaters for ${zip} => data/${zip}.theaters.json`);
-          });
+      get({
+        url: `https://www.moviepass.com/theaters/zip/${zip}`,
+        json: true,
+      }, (err, res, data) => {
+        const file = `data/${zip}.theaters.json`;
+        fs.writeFile(file, JSON.stringify(data, null, 2), (err) => {
+          if (err) throw err;
+          done++;
+          console.log(`${done}/${total}\tfetched theaters for ${zip} => ${file}`);
         });
       });
     })(zip);
